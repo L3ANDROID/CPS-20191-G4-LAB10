891f41b2a5986b17335b05513d4ad6930611145e
L3ANDROID <40245178+L3ANDROID@users.noreply.github.com>
2019-05-10 16:46:13 -0500

    primer comentario

---
+++ src/main/java/com/tecsup/lab10/controlador/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab10.controlador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
